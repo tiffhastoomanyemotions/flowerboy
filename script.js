e377e390deec4a15fb792a82e896a29d7bcbb294
tiffhastoomanyemotions <41089388+tiffhastoomanyemotions@users.noreply.github.com>
2019-10-06 14:29:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function goHome() {
+  window.location.href = "home.html";
+}
+
+function goBlog() {
+  window.location.href = "blog.html";
+}
+
+function goInspo() {
+  window.location.href = "inspirations.html";
+}
+
+function go2019() {
+  window.location.href = "poems2019.html";
+}
+
+function go2018() {
+  window.location.href = "poems2018.html";
+}
+
+function go2014() {
+  window.location.href = "poems2014.html";
+}
